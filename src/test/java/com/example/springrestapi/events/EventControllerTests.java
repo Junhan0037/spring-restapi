b046c5e187f1b8375c48ea911521e50d7aa18ce1
Junhan <wnsgks0037@naver.com>
2020-10-26 15:17:04 +0900

    ADD : 이벤트 조회 API 구현

--- src/test/java/com/example/springrestapi/events/EventControllerTests.java
+++ src/test/java/com/example/springrestapi/events/EventControllerTests.java
@@ -217,7 +217,7 @@ public class EventControllerTests {
         //given
         IntStream.range(0, 30).forEach(this::generateEvent);
 
-        //when
+        //when & then
         this.mockMvc.perform(get("/api/events")
                     .param("page", "1")
                     .param("size", "10")
@@ -232,12 +232,38 @@ public class EventControllerTests {
                 .andDo(document("query-document"));
     }
 
-    private void generateEvent(int index) {
+    @Test
+    @DisplayName("기존의 이벤트를 하나 조회하기")
+    public void getEvent() throws Exception {
+        //given
+        Event event = this.generateEvent(100);
+
+        //when & then
+        this.mockMvc.perform(get("/api/events/{id}", event.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").exists())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("_links.self").exists())
+                .andExpect(jsonPath("_links.profile").exists())
+                .andDo(document("get-an-event"));
+    }
+
+    @Test
+    @DisplayName("없는 이벤트로 조회했을 때 404 응답받기")
+    public void getEvent404() throws Exception {
+        //when & then
+        this.mockMvc.perform(get("/api/events/11883"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    private Event generateEvent(int index) {
         Event event = Event.builder()
                 .name("event" + index)
                 .description("test event")
                 .build();
-        this.eventRepository.save(event);
+        return this.eventRepository.save(event);
     }
 
 }
