5010f55df7f8a82f98bc9e70c68e83e4322715b7
Junhan <wnsgks0037@naver.com>
2020-10-27 21:39:14 +0900

    ADD : 스프링 시큐리티 적용

---
+++ src/test/java/com/example/springrestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.springrestapi.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void findByUsername() {
+        //given
+        String username = "junhan@email.com";
+        String password = "junhan";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        accountRepository.save(account);
+
+        //when
+        UserDetailsService userDetailsService = accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        //then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+}
