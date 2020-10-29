f5aca29dedeef0d14d7a734b1ff5dcb9dd47dd3e
Junhan <wnsgks0037@naver.com>
2020-10-29 20:36:51 +0900

    ADD : 스프링 시큐리티 기본 설정

--- src/main/java/com/example/springrestapi/SpringRestapiApplication.java
+++ src/main/java/com/example/springrestapi/SpringRestapiApplication.java
@@ -1,20 +1,13 @@
 package com.example.springrestapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestapiApplication.class, args);
-    }
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
     }
 
 }
